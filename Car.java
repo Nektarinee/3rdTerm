f3b9e02e13a1cda2858559064bc344842eb665f6
Nektarinee <71876309+Nektarinee@users.noreply.github.com>
2020-12-24 17:52:44 +0300

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,55 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Car implements Runnable
+{
+    AtomicInteger atomicInteger = new AtomicInteger(0);
+    private static int CARS_COUNT;
+    static {
+        CARS_COUNT = 0;
+    }
+    private Race race;
+    private int speed;
+    private String name;
+    private CyclicBarrier cyclicBarrier;
+    public String getName() {
+        return name;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public Car(Race race, int speed, CyclicBarrier cyclicBarrier) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+        this.cyclicBarrier = cyclicBarrier;
+    }
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int)(Math.random() * 800));
+            System.out.println(this.name + " готов");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            cyclicBarrier.await();
+            cyclicBarrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+        if (atomicInteger.incrementAndGet()==1)
+        {
+            System.out.println(name + " is the WINNER!");
+        }
+    }
+
+}
